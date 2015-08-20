045330311191a1a3bb3602e4cf6fc961178e565a
danielasamardgieva <samardzhievad@gmail.com>
2015-08-20 09:45:04 +0300

    Modul02 Video4 Променливи от тип string

--- Modul2/Modul2/Program.cs
+++ Modul2/Modul2/Program.cs
@@ -7,8 +7,14 @@ namespace Modul2
 		public static void Main (string[] args)
 		{
 			//Дефиниране на променливи
-			int a = 0, b = 0;
-		
+			int a = 0, b = 0, sum=0;
+			string programe_name = "Modul02 - Примери";
+			string programe_version = "1.0";
+
+			//За програмата
+			Console.WriteLine (programe_name);
+			Console.WriteLine ("Версия: " +  programe_version + "\n\n");
+
 			//Събиране на числа
 			a = 10;
 			b = 5;
@@ -16,9 +22,16 @@ namespace Modul2
 			Console.WriteLine (a);
 			Console.Write (" b = ");
 			Console.WriteLine (b);
-			Console.WriteLine ("/n Резултат а+в=");
-			a += b;
+			sum = a + b;
+			Console.Write ("\n Сумата е ----> sum=");
+			Console.WriteLine (sum + "\n\n");
+			Console.Write (" a = ");
 			Console.WriteLine (a);
+			Console.Write (" b = ");
+			Console.WriteLine (b);
+			Console.Write ("Резултат а+в=");
+			a += b;
+			Console.WriteLine (a + "\n\n");
 
 			//Изваждане на числа
 			a = 15;
@@ -27,9 +40,9 @@ namespace Modul2
 			Console.WriteLine (a);
 			Console.Write (" b = ");
 			Console.WriteLine (b);
-			Console.WriteLine ("/n Резултат а-в=");
+			Console.Write ("\n Резултат а-в=");
 			a -= b;
-			Console.WriteLine (a);
+			Console.WriteLine (a + "\n\n");
 		
 			//Умножение на числа
 			a = 15;
@@ -38,9 +51,9 @@ namespace Modul2
 			Console.WriteLine (a);
 			Console.Write (" b = ");
 			Console.WriteLine (b);
-			Console.WriteLine ("/n Резултат а*в=");
+			Console.Write ("\n Резултат а*в=");
 			a *= b;
-			Console.WriteLine (a);
+			Console.WriteLine (a + "\n\n");
 
 			//Стандартно=Целочислено деление на числа
 			a = 15;
@@ -49,8 +62,8 @@ namespace Modul2
 			Console.WriteLine (a);
 			Console.Write (" b = ");
 			Console.WriteLine (b);
-			Console.WriteLine ("/n Резултат а/в=");
-			Console.WriteLine (a / b);
+			Console.Write ("\n Резултат а/в=");
+			Console.WriteLine (a / b + "\n\n");
 		     
 			//Хващане на остатъка от целочислено деление 
 			a = 15;
@@ -59,16 +72,16 @@ namespace Modul2
 			Console.WriteLine (a);
 			Console.Write (" b = ");
 			Console.WriteLine (b);
-			Console.WriteLine ("/n Резултат а%в=");
-			Console.WriteLine (a % b);
+			Console.Write ("\n Резултат а%в=");
+			Console.WriteLine (a % b + "\n\n");
 
 			//Легендарното ++
 			a=15;
 			Console.Write (" /n Стара стойност а= ");
 			Console.WriteLine (a);
-			Console.WriteLine (" /n Използване на ++ ");
+			Console.WriteLine (" \n Използване на а++ ");
 			a++;
-			Console.Write (" /n Нова стойност а= ");
+			Console.Write (" \n Нова стойност а= ");
 			Console.WriteLine (a);
 	     	}
 		}
