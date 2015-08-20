142ddf1b0a103fd9609b20d4bf7939344bb0fd19
danielasamardgieva <samardzhievad@gmail.com>
2015-08-20 09:08:01 +0300

    Модул 02 Видео 03 Основни оператори за целочислен тип данни

--- Modul2/Modul2/Program.cs
+++ Modul2/Modul2/Program.cs
@@ -7,27 +7,69 @@ namespace Modul2
 		public static void Main (string[] args)
 		{
 			//Дефиниране на променливи
-			int a=0, b=0, c=0;
+			int a = 0, b = 0;
 		
 			//Събиране на числа
-			a=5;
+			a = 10;
+			b = 5;
+			Console.Write (" a = ");
+			Console.WriteLine (a);
+			Console.Write (" b = ");
+			Console.WriteLine (b);
+			Console.WriteLine ("/n Резултат а+в=");
+			a += b;
+			Console.WriteLine (a);
+
+			//Изваждане на числа
+			a = 15;
 			b = 10;
 			Console.Write (" a = ");
-			Console.Write (a);
+			Console.WriteLine (a);
 			Console.Write (" b = ");
-			Console.Write (b);
-
-			//Смяна на стойностите
-			c=a;
-			a = b;
-			b=c;
-
-			Console.Write (" Смяна на стойностите -> a = ");
-			Console.Write (a);
-			Console.Write (" b = ");
-			Console.Write (b);
+			Console.WriteLine (b);
+			Console.WriteLine ("/n Резултат а-в=");
+			a -= b;
+			Console.WriteLine (a);
 		
+			//Умножение на числа
+			a = 15;
+			b = 10;
+			Console.Write (" a = ");
+			Console.WriteLine (a);
+			Console.Write (" b = ");
+			Console.WriteLine (b);
+			Console.WriteLine ("/n Резултат а*в=");
+			a *= b;
+			Console.WriteLine (a);
 
+			//Стандартно=Целочислено деление на числа
+			a = 15;
+			b = 10;
+			Console.Write (" a = ");
+			Console.WriteLine (a);
+			Console.Write (" b = ");
+			Console.WriteLine (b);
+			Console.WriteLine ("/n Резултат а/в=");
+			Console.WriteLine (a / b);
+		     
+			//Хващане на остатъка от целочислено деление 
+			a = 15;
+			b = 10;
+			Console.Write (" a = ");
+			Console.WriteLine (a);
+			Console.Write (" b = ");
+			Console.WriteLine (b);
+			Console.WriteLine ("/n Резултат а%в=");
+			Console.WriteLine (a % b);
+
+			//Легендарното ++
+			a=15;
+			Console.Write (" /n Стара стойност а= ");
+			Console.WriteLine (a);
+			Console.WriteLine (" /n Използване на ++ ");
+			a++;
+			Console.Write (" /n Нова стойност а= ");
+			Console.WriteLine (a);
+	     	}
 		}
 	}
-}
