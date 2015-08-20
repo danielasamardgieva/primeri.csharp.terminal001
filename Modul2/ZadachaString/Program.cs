012227f538de311fc37bb8d31a974fee694f1ff0
danielasamardgieva <samardzhievad@gmail.com>
2015-08-20 16:58:42 +0300

    Модул02 Видео06 - Задачи

---
+++ Modul2/ZadachaString/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ZadachaString
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Задача 01
+			//Дефиниране на променливи от тип стринг
+			string first = "Hello";
+			string second = "World";
+
+			//Събиране на променливи от тип стринг в нов обект
+			object concatenatedString = first + " " + second;
+			//нов стринг
+			string newString = (string)concatenatedString;
+			Console.WriteLine (newString);
+
+
+			//Задача02
+			//Дефиниране на променливи 
+			int a = 0;
+
+			string bb = "BBB";
+
+			//Въвеждане на стойности от ком.ред
+			Console.Write ("Моля въведете а:");
+			a=Console.Read ();
+			string aa = Convert.ToString (a);
+
+			Console.Write ("Моля въведете дума:");
+			bb= Console.ReadLine ();
+			Console.WriteLine (bb);
+
+			//Резултат
+
+			Console.WriteLine ("Нов израз: "+aa);
+
+		}
+
+
+		}
+
+	}
+
+
+ 
