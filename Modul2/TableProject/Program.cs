904d712f8521d930370cdccd6bdd3047191bddaa
danielasamardgieva <samardzhievad@gmail.com>
2015-08-21 10:25:16 +0300

    Модул02 Видео08 - Таблица

--- Modul2/TableProject/Program.cs
+++ Modul2/TableProject/Program.cs
@@ -6,21 +6,38 @@ namespace TableProject
 	{
 		public static void Main (string[] args)
 		{
-			string[] row = new string [3];
-			row[0]="Ред 1";
-			row[1]="Ред 2";
-			row[2]="Ред 3";
-			Console.WriteLine ("Масив row: " + row [0] + ", " + row [1] + ", " + row [2] + "\n\n");
+//			Декларирам таблицас 2 реда и 3 колони
+			string[,]table=new string[2,3];
+			//Въвеждам данни за 1 ред:
+			table[0,0]="Мартин";table[0,1]="Симеонов";table[0,2]="31";
 
-			//Дефинирам масив parse с разделител ,
-			string [] parse = "a,b,c,d,e,f".Split(',');
+			//Въвеждам данни за 2 ред:
+			table[1,0]="Симеон";table[1,1]="Мартинов";table[1,2]="41";
 
-			//Преброявам елементите в масива
-			Console.WriteLine( "Броят на елементите в масива: \n" +"a,b,c,d,e,f \n\n e равен на: -->  "+parse.Length);
+			//Извеждаме 1-ви символ от 1-ви елемент, 2-ри и 3-ти ел-т:
+			//Console.WriteLine((table[0,0])[0]+". "+table[0,1]+", "+table[0,2]);
 
-			//Събирам елементите на масива в стринг
-			string list1=string.Join(";",parse);
-			Console.WriteLine( "Новият стринг е: \n"+list1+"\n\n");
+			//Извеждам ред по избор от клавиатурата (1-ви или 2-ри)
+			Console.Write("Кой ред искате да видите: ");
+			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
+			Console.WriteLine((table[_index,0])[0]+". "+table[_index,1]+", "+table[_index,2]);
+
+
+//          string[] row = new string [3];
+//			row[0]="Ред 1";
+//			row[1]="Ред 2";
+//			row[2]="Ред 3";
+//			Console.WriteLine ("Масив row: " + row [0] + ", " + row [1] + ", " + row [2] + "\n\n");
+//
+//			//Дефинирам масив parse с разделител ,
+//			string [] parse = "a,b,c,d,e,f".Split(',');
+//
+//			//Преброявам елементите в масива
+//			Console.WriteLine( "Броят на елементите в масива: \n" +"a,b,c,d,e,f \n\n e равен на: -->  "+parse.Length);
+//
+//			//Събирам елементите на масива в стринг
+//			string list1=string.Join(";",parse);
+//			Console.WriteLine( "Новият стринг е: \n"+list1+"\n\n");
 		}
 	}
 }
