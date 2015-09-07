e10ecebd4d69dac7f2263900989755e2f37ab53c
danielasamardgieva <samardzhievad@gmail.com>
2015-09-07 21:59:03 +0300

    Модул3 Видео10 Улавяне на грешки с try catch

---
+++ ModulNew03/BoolTryCatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BoolTryCatch
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] _i = new int[] { 4, 3, 2 };
+
+			int _ui = 0;
+			string _input = "0";
+
+			Console.WriteLine ("Въведете номер на елемент в редицата-->");
+			_input = Console.ReadLine ();
+
+			bool _check01 = int.TryParse (_input, out _ui);
+
+			try
+				{
+				Console.WriteLine("Елемент "+_ui.ToString()+" от масива е:");
+					Console.WriteLine (_i [_ui - 1]);
+			}catch{
+				Console.WriteLine (" <недефиниран елемент> ");
+				} 
+
+		}
+	}
+}
