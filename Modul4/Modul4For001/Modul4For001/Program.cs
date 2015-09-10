a46c2ca4ee24dc2ac4108263b4a70f97be5e8e05
danielasamardgieva <samardzhievad@gmail.com>
2015-09-10 17:22:08 +0300

    Модул4 Видео5 Пример с do while

--- Modul4/Modul4For001/Modul4For001/Program.cs
+++ Modul4/Modul4For001/Modul4For001/Program.cs
@@ -29,17 +29,30 @@ namespace Modul4For001
 //			}
 
 			//Пример с While
-			int i=0;
-			while (i<_table.Length/3)
-			  {
-				
+//			int i=0;
+//			while (i<_table.Length/3)
+//			  {
+//				
+//				Console.WriteLine
+//				(
+//					_table [i, 0].PadRight (12)+
+//					_table [i, 1].PadRight (12)+
+//					_table [i, 2].PadRight (12));
+//			             i++;
+//			  }
+
+			//пример с do while
+			 int i=0;
+			do{
+				//код
 				Console.WriteLine
-				(
-					_table [i, 0].PadRight (12)+
-					_table [i, 1].PadRight (12)+
-					_table [i, 2].PadRight (12));
-			             i++;
-			  }
+						(_table [i, 0].PadRight (12)+
+						 _table [i, 1].PadRight (12)+
+						 _table [i, 2].PadRight (12));
+					             i++;
+			}
+
+			while (i<_table.Length/3);
 		}
 	}
 }
