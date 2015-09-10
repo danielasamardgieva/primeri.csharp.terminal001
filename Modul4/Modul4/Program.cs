383f22bef1d550d18444bf2e64fb9934b81fd2e4
danielasamardgieva <samardzhievad@gmail.com>
2015-09-10 16:16:45 +0300

    Модул4 Видео3 Примери за влагане на цикли

---
+++ Modul4/Modul4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Modul4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//for(int i=0;i<5;i++)
+			//			{
+			//				//код на цикъла за четене на индекс i
+			//				Console.WriteLine (i.ToString());
+			//			} 
+
+			string[] _table = { "Иван", "Гошо", "Пешо" };
+			//Антетка на тавлицата
+			Console.WriteLine("Име");
+			Console.WriteLine("==================================");
+
+			//Тяло на таблицата
+			for (int i = 0; i < _table.Length; i++) {
+				//Код на цикъла
+				Console.WriteLine (_table [i]);
+			}
+		}
+	}
+}
---
+++ Modul4/VlaganeNaFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VlaganeNaFor
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string[,] _table = 
+			{
+				{"Иван","Георгиев","19"},
+				{"Петър","Маринов","24"},
+				{"Филип","Николов","45"},
+				{"Валя","Стоянова","32"},
+				{"Асен","Серафимов","51"},
+				{"Митко","Димитров","25"}
+			};
+
+			//Антетка
+
+			Console.WriteLine("Име".PadRight(12)+"Фамилия".PadRight(12)+"Години".PadRight(12));
+			Console.WriteLine("================================================");
+
+			//Тяло на таблицата
+
+			for (int i = 0; i < _table.Length / 3; i++) 
+				{
+				for (int j = 0; j < 3; j++)
+				{
+					
+					Console.Write(_table [i, j].PadRight (12));
+
+				}
+				Console.WriteLine ();
+			}
+		}
+	}
+}
