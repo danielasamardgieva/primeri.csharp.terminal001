2a635053203780c5a9f35a7481f60cee7ab3d4dc
danielasamardgieva <samardzhievad@gmail.com>
2015-09-10 05:43:17 +0300

    Модул4 Видео2  Пример с оператор For

--- ModulNew03/OperatorFor/Program.cs
+++ ModulNew03/OperatorFor/Program.cs
@@ -6,7 +6,23 @@ namespace OperatorFor
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+//			for(int i=0;i<5;i++)
+//			{
+//				//код на цикъла за четене на индекс i в прав ред
+//				Console.WriteLine (i.ToString());
+//			} 
+//
+//			for(int i=5;i>0;i--)
+//				{
+//				//код на цикъла за четене на индекс i в обратен ред
+//				Console.WriteLine (i.ToString());
+//			    } 
+
+			for(int i=0;i<=5;i+=2)
+				{
+				//код на цикъла за четене на четните индекси i
+				Console.WriteLine (i.ToString());
+				} 
 		}
 	}
 }
