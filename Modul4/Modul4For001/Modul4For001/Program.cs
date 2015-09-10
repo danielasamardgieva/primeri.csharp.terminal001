2e9a265b7065d18a979a3ca48bd00c3fd6c399cd
danielasamardgieva <samardzhievad@gmail.com>
2015-09-10 17:07:41 +0300

    Модул4 Видео4 Пример с while

--- Modul4/Modul4For001/Modul4For001/Program.cs
+++ Modul4/Modul4For001/Modul4For001/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace VlaganeNaFor
+namespace Modul4For001
 {
 	class MainClass
 	{
@@ -15,24 +15,34 @@ namespace VlaganeNaFor
 				{"Асен","Серафимов","51"},
 				{"Митко","Димитров","25"}
 			};
-
 			//Антетка
-
 			Console.WriteLine("Име".PadRight(12)+"Фамилия".PadRight(12)+"Години".PadRight(12));
 			Console.WriteLine("================================================");
-
 			//Тяло на таблицата
+//			for (int i = 0; i < _table.Length / 3; i++)
+//			{
+//				for (int j = 0; j < 3; j++) 
+//				{
+//					Console.Write(_table [i, j].PadRight (12));
+//				}
+//				Console.WriteLine ();
+//			}
 
-			for (int i = 0; i < _table.Length / 3; i++) 
-				{
-				for (int j = 0; j < 3; j++)
-				    {
-					
-     			     Console.Write(_table [i, j].PadRight(12));
+			//Пример с While
+			int i=0;
+			while (i<_table.Length/3)
+			  {
 				
-				     }
-				  Console.WriteLine ();
-			    }
+				Console.WriteLine
+				(
+					_table [i, 0].PadRight (12)+
+					_table [i, 1].PadRight (12)+
+					_table [i, 2].PadRight (12));
+			             i++;
+			  }
 		}
 	}
 }
+
+			
+				
