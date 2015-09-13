3d2887f4f308e0fc17689eaa1af2d3a3bef79f69
danielasamardgieva <samardzhievad@gmail.com>
2015-09-13 16:06:27 +0300

    Модул4 Видео6 Пример с do...while

---
+++ Modul4/napred2nazad1do10/napred2nazad1do10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace napred2nazad1do10
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//покажете napred 2 nazad 1do 10
+			int i=0;
+			bool napred=true;
+			while(i < 10) {
+				if (napred) {
+					napred=false;
+					i+=2;
+				} else {
+					napred=true;
+					i--;
+				}
+				Console.WriteLine(i);
+		
+
+		}
+	}
+}
---
+++ Modul4A/Modul4a/Modul4a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modul4a
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//definicia
+			string _userinput = Console.Write ("?");
+
+			//potrebitelski vhod
+			do{
+			_userinput = Console.ReadLine ();
+		} while (_userinput !="exit");
+	    }
+		
+	}
+}
