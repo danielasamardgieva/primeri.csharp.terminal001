3ec85d784effb74b452bbdbfd7536e0f0e779e69
danielasamardgieva <samardzhievad@gmail.com>
2015-09-17 06:04:07 +0300

    Модул5 Видео1 Методи - въведение

---
+++ Modul5/Modul5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Modul5
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниции
+			string usercomand = " ";
+
+			//потребителски команди
+			do {
+				Console.Write ("$ "); usercomand=Console.ReadLine ();
+
+				if (usercomand.Contains ("cmd01")) cmd01 (usercomand);
+				if (usercomand.Contains ("cmd02")) cmd02 (usercomand);
+				} while(usercomand !="exit");
+		   }
+			public static void cmd01(string _input) //cmd01 5 -->5^2
+			{ 
+			try
+				{
+				double _temp = 0;
+				if (double.TryParse (_input.Split (' ') [1], out _temp)) 
+					{
+					Console.WriteLine (("Резултат на втора степен--->" + (_temp * _temp).ToString ()));
+					}
+				} catch {
+
+						}
+			}
+			public static int cmd02(string _input) //cmd2 командата повдига на 3-та степен числото след нея
+				{
+			int _temp = 0;
+			if (int.TryParse (_input.Split (' ') [1], out _temp))
+					{
+				Console.WriteLine("Резултат на трета степен ---------->"+(_temp * _temp*_temp).ToString ());
+					}
+			return _temp;
+				}
+	}
+}
