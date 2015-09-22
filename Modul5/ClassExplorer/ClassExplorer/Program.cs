18327a794a3c3d028b59c61ad18bc9bdde853a95
danielasamardgieva <samardzhievad@gmail.com>
2015-09-22 08:43:14 +0300

    Модул5 Видео4 Въведение в класовете

---
+++ Modul5/ClassExplorer/ClassExplorer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassExplorer
+{
+	public class person
+	{
+		public string simpleString="тестова променлива от клас";
+
+		public person ()
+		{
+			simpleString = "Промяна на променливата при инициализация";
+		}
+
+
+
+	}
+
+
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			//Дефиниране на клас
+			person _person=new person ();
+
+			//Достъпване на клас
+			Console.WriteLine (_person.simpleString);
+		}
+	}
+}
