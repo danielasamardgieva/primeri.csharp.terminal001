f336c3e69bd5d7c43a29f0660d3f1dd4bd88e4a9
danielasamardgieva <samardzhievad@gmail.com>
2015-09-22 10:39:52 +0300

    Модул5 Видео5 Дефиниране на променливи в клас

--- Modul5/ClassExplorer/ClassExplorer/Program.cs
+++ Modul5/ClassExplorer/ClassExplorer/Program.cs
@@ -1,31 +1,39 @@
-﻿using System;
+using System;
 
 namespace ClassExplorer
 {
 	public class person
 	{
 		public string simpleString="тестова променлива от клас";
+		public string user= "";
 
-		public person ()
+		private string hiddenString= "";
+
+		public person (string _ini)
 		{
-			simpleString = "Промяна на променливата при инициализация";
+			if (_ini == "pass1")hiddenString = " Потребител 1";
+			if (_ini == "pass2")hiddenString = " Потребител 2";
+
+			user = hiddenString;
+
+			if (hiddenString.Length > 0)
+				simpleString = "Добре дошли отново " + user + "!";
+			else
+			simpleString = "Вие нямате достъп до тази програма!";
 		}
-
-
-
 	}
 
-
-	class MainClass
+		class MainClass
 	{
-		
 		public static void Main (string[] args)
-		{
+		{Console.Write ("Парола:");
+			string _user = Console.ReadLine ();
 			//Дефиниране на клас
-			person _person=new person ();
-
+			person _person=new person (_user);
 			//Достъпване на клас
 			Console.WriteLine (_person.simpleString);
+			
 		}
 	}
+
 }
