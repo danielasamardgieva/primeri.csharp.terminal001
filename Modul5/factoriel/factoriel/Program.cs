32c6dcac51bbe2a829892ed57eba866de1158ef0
danielasamardgieva <samardzhievad@gmail.com>
2015-10-01 18:05:48 +0300

    Модул5 Видео8 Рекурсия-пример с факториел

---
+++ Modul5/factoriel/factoriel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace factoriel
+{
+	class MainClass
+	{
+		public static int Fact(int _fact)
+		{
+			if (_fact <= 1)
+				return 1;
+			else
+				return _fact * Fact (_fact - 1);
+		}
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (Fact (3));
+			Console.WriteLine (Fact (4));
+			Console.WriteLine (Fact (5));
+		}
+	}
+}
